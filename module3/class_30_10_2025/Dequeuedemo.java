6ffbcdf31d9cc7f740492bf0b083cbc31b1c6855
Arpit000009 <100606591+Arpit000009@users.noreply.github.com>
2025-10-30 17:11:29 +0530

    dequeue

---
+++ module3/class_30_10_2025/Dequeuedemo.java
@@ -0,0 +1,22 @@
+package module3.class_30_10_2025;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Dequeuedemo {
+    public static void main(String[] args) {
+        Deque<String>dq = new ArrayDeque<String>();
+        dq.addFirst("java");
+        dq.addLast("Python");
+        dq.offerFirst("C++");
+        dq.offerLast("JavaScript");
+        System.out.println("dequeue after "+ dq);
+        System.out.println("first element: "+dq.peek());
+        System.out.println("last element: "+dq.peekLast());
+
+        dq.removeFirst();
+        dq.removeLast();
+        dq.remove();
+        System.out.println("dequeue after removal"+ dq);
+    }
+}
